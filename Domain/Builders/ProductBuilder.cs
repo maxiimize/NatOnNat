7d3254b6fc4ee73c716145de8c8b0f7178ab4dd2
maxiimize <max.b@hotmail.se>
2025-10-10 20:59:38 +0200

    Make ProductBuilder public and implement IProductBuilder
    Refactored `ProductBuilder` to implement the `IProductBuilder`
    interface and made it public for external access. Added a
    private `_product` field and several fluent methods to set
    properties like `Id`, `Name`, `Description`, `Price`,
    `ImageUrl`, `Category`, `StockQuantity`, `IsFavorite`, and
    `CreatedDate`. Introduced a `Build()` method with validation
    logic to ensure required fields are set and defaults are
    applied. Updated `using` directives to include necessary
    namespaces.

--- Domain/Builders/ProductBuilder.cs
+++ Domain/Builders/ProductBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,76 @@ using System.Threading.Tasks;
 
 namespace Domain.Builders
 {
-    internal class ProductBuilder
+    public class ProductBuilder : IProductBuilder
     {
+        private Product _product = new();
+
+        public IProductBuilder WithId(int id)
+        {
+            _product.Id = id;
+            return this;
+        }
+
+        public IProductBuilder WithName(string name)
+        {
+            _product.Name = name;
+            return this;
+        }
+
+        public IProductBuilder WithDescription(string description)
+        {
+            _product.Description = description;
+            return this;
+        }
+
+        public IProductBuilder WithPrice(decimal price)
+        {
+            _product.Price = price;
+            return this;
+        }
+
+        public IProductBuilder WithImageUrl(string imageUrl)
+        {
+            _product.ImageUrl = imageUrl;
+            return this;
+        }
+
+        public IProductBuilder WithCategory(string category)
+        {
+            _product.Category = category;
+            return this;
+        }
+
+        public IProductBuilder WithStockQuantity(int quantity)
+        {
+            _product.StockQuantity = quantity;
+            return this;
+        }
+
+        public IProductBuilder AsFavorite(bool isFavorite = true)
+        {
+            _product.IsFavorite = isFavorite;
+            return this;
+        }
+
+        public IProductBuilder WithCreatedDate(DateTime createdDate)
+        {
+            _product.CreatedDate = createdDate;
+            return this;
+        }
+
+        public Product Build()
+        {
+            if (string.IsNullOrWhiteSpace(_product.Name))
+                throw new InvalidOperationException("Product name is required");
+
+            if (_product.Price < 0)
+                throw new InvalidOperationException("Product price cannot be negative");
+
+            if (_product.CreatedDate == default)
+                _product.CreatedDate = DateTime.Now;
+
+            return _product;
+        }
     }
 }
